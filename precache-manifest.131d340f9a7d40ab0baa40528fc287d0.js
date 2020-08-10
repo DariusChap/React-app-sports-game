58b9f150b1ca170c0ddfeca8ebc21184134bfad9
Darius <chapd289@robertmorris.edu>
2020-08-10 15:03:28 -0500

    Updates

---
+++ precache-manifest.131d340f9a7d40ab0baa40528fc287d0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a180036227fe590c2e1b1f6505a01cb6",
+    "url": "/React-app-sports-game/index.html"
+  },
+  {
+    "revision": "4f9ceca3c801cd16edf2",
+    "url": "/React-app-sports-game/static/css/main.bf0bc4e5.chunk.css"
+  },
+  {
+    "revision": "ca5f7046587bc7c9e36b",
+    "url": "/React-app-sports-game/static/js/2.e3c81512.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-app-sports-game/static/js/2.e3c81512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9ceca3c801cd16edf2",
+    "url": "/React-app-sports-game/static/js/main.b4ce4cbe.chunk.js"
+  },
+  {
+    "revision": "523790456f13543f3827",
+    "url": "/React-app-sports-game/static/js/runtime-main.422c02b6.js"
+  },
+  {
+    "revision": "6247aedc5582f98b84c39430cc7de3ca",
+    "url": "/React-app-sports-game/static/media/Basketball_Bounce_Sound_Effect.6247aedc.mp3"
+  },
+  {
+    "revision": "89703d43610699831fff63f05d6b4e78",
+    "url": "/React-app-sports-game/static/media/Basketball_Net_Swish_Sound_Effect.89703d43.mp3"
+  },
+  {
+    "revision": "a61e9787afee6ff0564deba671776750",
+    "url": "/React-app-sports-game/static/media/CP3-Stealers.a61e9787.jpg"
+  },
+  {
+    "revision": "f69610dac4ccf5e55aba11099e6ed57f",
+    "url": "/React-app-sports-game/static/media/Lebron-blockers.f69610da.jpeg"
+  },
+  {
+    "revision": "dfec87d08c4a5b765ff5dc42f7b9b8cc",
+    "url": "/React-app-sports-game/static/media/MJ-Dunkers.dfec87d0.jpg"
+  },
+  {
+    "revision": "a86fee25e62e053817d27e5def443218",
+    "url": "/React-app-sports-game/static/media/Pacman_Eating_Cherry_Sound_Effect.a86fee25.mp3"
+  },
+  {
+    "revision": "e1de8abe3a8309ba079fc337d2cb3660",
+    "url": "/React-app-sports-game/static/media/Steph-Shooters.e1de8abe.jpg"
+  }
+]);
